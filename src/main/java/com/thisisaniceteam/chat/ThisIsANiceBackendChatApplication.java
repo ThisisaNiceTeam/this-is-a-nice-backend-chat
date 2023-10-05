19a4656b03ee01391caa61aa031f4da5c5f788c1
hwan2da <dla612@naver.com>
2023-10-05 11:17:20 +0900

    init

---
+++ src/main/java/com/thisisaniceteam/chat/ThisIsANiceBackendChatApplication.java
@@ -0,0 +1,13 @@
+package com.thisisaniceteam.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsANiceBackendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisIsANiceBackendChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thisisaniceteam/chat/ThisIsANiceBackendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisisaniceteam.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisIsANiceBackendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
