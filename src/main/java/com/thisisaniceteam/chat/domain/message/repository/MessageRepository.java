74289e50dd47fd961615ff80db9c369ad131854a
hwan2da <dla612@naver.com>
2023-10-27 14:31:33 +0900

    feat: WebSocket Configuration 추가

--- src/main/java/com/thisisaniceteam/chat/domain/message/repository/MessageRepository.java
+++ src/main/java/com/thisisaniceteam/chat/domain/message/repository/MessageRepository.java
@@ -1,10 +1,9 @@
 package com.thisisaniceteam.chat.domain.message.repository;
 
 import com.thisisaniceteam.chat.model.Message;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends MongoRepository<Message, ObjectId> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
