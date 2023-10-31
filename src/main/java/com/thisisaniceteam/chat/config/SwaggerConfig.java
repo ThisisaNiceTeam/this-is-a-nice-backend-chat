edeaae4cf3206b8799bd1726d9f1d068dde2365e
hwan2da <dla612@naver.com>
2023-10-31 15:37:31 +0900

    feat: swagger configuration 작성

---
+++ src/main/java/com/thisisaniceteam/chat/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.thisisaniceteam.chat.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .externalDocs(null)
+                .servers(null)
+                .security(null)
+                .tags(null)
+                .paths(null)
+                .components(new Components());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("this-is-a-nice-backend-chat")
+                .description("Chat API 명세서")
+                .contact(new Contact()
+                        .name("LimSeHwan")
+                        .url("https://github.com/Torres-09")
+                        .email("dla612@naver.com"))
+                .license(new License())
+                .version("1.0.0");
+    }
+}
