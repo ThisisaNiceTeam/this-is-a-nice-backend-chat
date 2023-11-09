e13ddf0a5374d25ef388eb86179f64d5b412fabd
hwan2da <dla612@naver.com>
2023-11-09 15:53:02 +0900

    feat: TCP handShakeInterceptor 등록
    TCP 연결시 작동하는 handshake

---
+++ src/main/java/com/thisisaniceteam/chat/common/interceptor/HandShakeInterceptor.java
@@ -0,0 +1,27 @@
+package com.thisisaniceteam.chat.common.interceptor;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+public class HandShakeInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        if (request instanceof ServletServerHttpRequest servletServerHttpRequest) {
+            HttpSession session = servletServerHttpRequest.getServletRequest().getSession();
+            attributes.put("session", session);
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
