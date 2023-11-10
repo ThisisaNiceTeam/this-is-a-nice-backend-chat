8444ef0155297de0ce876764b6b4f3db5a0e8e05
hwan2da <dla612@naver.com>
2023-11-11 00:03:01 +0900

    feat: WebSocketConfiguration 추가
    WebSocketConfiguration을 구현하는 Socket Config 파일을 추가합니다.
    handShake에 대한 인터셉터와 WebSocket Handler를 추가하고 CORS를 허용합니다.

--- src/main/java/com/thisisaniceteam/chat/common/config/WebSocketConfig.java
+++ src/main/java/com/thisisaniceteam/chat/common/config/WebSocketConfig.java
@@ -1,22 +1,23 @@
 package com.thisisaniceteam.chat.common.config;
 
+import com.thisisaniceteam.chat.common.handler.WebSocketHandler;
+import com.thisisaniceteam.chat.common.interceptor.HandShakeInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-    }
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+    private final HandShakeInterceptor handShakeInterceptor;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(webSocketHandler, "/ws/chat")
+                .addInterceptors(handShakeInterceptor)
+                .setAllowedOrigins("*");
     }
 }
