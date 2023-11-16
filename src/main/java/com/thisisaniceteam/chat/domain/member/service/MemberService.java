7daea6b1035167d81b2ad37f74c8139b2b7ec102
hwan2da <dla612@naver.com>
2023-11-16 17:54:35 +0900

    feat: 회원 컴포넌트 추가

---
+++ src/main/java/com/thisisaniceteam/chat/domain/member/service/MemberService.java
@@ -0,0 +1,4 @@
+package com.thisisaniceteam.chat.domain.member.service;
+
+public interface MemberService {
+}
---
+++ src/main/java/com/thisisaniceteam/chat/domain/member/service/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package com.thisisaniceteam.chat.domain.member.service;
+
+import com.thisisaniceteam.chat.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository;
+
+}
